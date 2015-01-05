cbe0bd5803873d092b58b682b8c2133993fe08ce
rsaat <rsaat@usa.com>
2015-01-05 15:38:14 -0200

    SocketServer implementation Episode5

--- src/cleancoderscom/socketserver/SocketServer.cs
+++ src/cleancoderscom/socketserver/SocketServer.cs
@@ -3,99 +3,92 @@ using System.Net;
 using System.Net.Sockets;
 using System.Threading;
 
+//Conversion from Java to C# 
+//Java Socket replaced with .NET TcpClient
+//Java ServerSocket replaced with .NET TcpListener
+
 namespace cleancoderscom.socketserver
 {
 
-	public class SocketServer
-	{
-	  private readonly int port;
-	  private readonly SocketService service;
-	  private bool running;
+    public class SocketServer
+    {
+        private readonly int port;
+        private readonly SocketService service;
+        private bool running;
 
-      /// Java ServerSocket replaced with .NET TcpListener  
-      private TcpListener serverSocket;
-	
-	 public SocketServer(int port, SocketService service)
-	 {
-		this.port = port;
-		this.service = service;
-        var ipAddress = Dns.GetHostEntry("localhost").AddressList[0];
-        serverSocket = new TcpListener(ipAddress,port);
-	 }
+        private TcpListener serverSocket;
 
-	 public virtual int Port
-	 {
-		  get
-		  {
-			return port;
-		  }
-	 }
+        public SocketServer(int port, SocketService service)
+        {
+            this.port = port;
+            this.service = service;
+            var ipAddress = Dns.GetHostEntry("localhost").AddressList[0];
+            serverSocket = new TcpListener(ipAddress, port);
+            serverSocket.Start();
+        }
 
-	  public virtual SocketService Service
-	  {
-		  get
-		  {
-			return service;
-		  }
-	  }
+        public virtual int Port
+        {
+            get
+            {
+                return port;
+            }
+        }
 
-	  public virtual void start()
-	  {
-        //call run method RunnableAnonymousInnerClassHelper in another thread 
-        //It is not exact replacemnt of Java Executors.newFixedThreadPool(4);  
-	    var runableHelpper = new RunnableAnonymousInnerClassHelper(this);
-	    ThreadPool.QueueUserWorkItem(runableHelpper.ThreadPoolCallback);  
-		running = true;
-	  }
+        public virtual SocketService Service
+        {
+            get
+            {
+                return service;
+            }
+        }
 
-	  private class RunnableAnonymousInnerClassHelper 
-	  {
-		  private readonly SocketServer outerInstance;
+        public virtual void start()
+        {
+            running = true;
 
-		  public RunnableAnonymousInnerClassHelper(SocketServer outerInstance)
-		  {
-			  this.outerInstance = outerInstance;
-		  }
+            ThreadPool.QueueUserWorkItem(
+                (o) =>
+                {
 
-	      public void ThreadPoolCallback(Object threadContext)
-	      {
-              run();
-	      }
+                    try {
+                        while (running)
+                        {
+                            var serviceSocket = serverSocket.AcceptTcpClient();
+                            ThreadPool.QueueUserWorkItem(
+                                (o1) =>
+                                {
+                                    service.serve(serviceSocket);
+                                });
+                        }
 
-	      public virtual void run()
-		  {
-			try
-			{
-              outerInstance.serverSocket.Start();
-			  var serviceSocket = outerInstance.serverSocket.AcceptTcpClient();
-			  outerInstance.service.serve(serviceSocket);
-			}
-            catch (Exception e)
-			{
-			  if (outerInstance.running)
-			  {
-				Console.WriteLine(e.ToString());
-				Console.Write(e.StackTrace);
-			  }
-			}
-		  }
-	  }
+                    }
+                    catch (Exception e)
+                    {
+                        if (running)
+                          Console.WriteLine(e.StackTrace);
+                    }
+                 }
+                 );
+            
+        }
 
-	  public virtual bool Running
-	  {
-		  get
-		  {
-			return running;
-		  }
-	  }
+       
+        public virtual bool Running
+        {
+            get
+            {
+                return running;
+            }
+        }
 
-	  public virtual void stop()
-	  {
-		//executor.awaitTermination(500, TimeUnit.MILLISECONDS);
-        System.Threading.Thread.Sleep(500);
-		serverSocket.Stop();
-		running = false;
-	  }
-	}
+        public virtual void stop()
+        {
+            running = false;
+            System.Threading.Thread.Sleep(250); //wait to finalize threads. 
+            serverSocket.Stop();
+          
+        }
+    }
 
 }
--- test/cleancoderscom/tests/socketserver/SocketServerTest.cs
+++ test/cleancoderscom/tests/socketserver/SocketServerTest.cs
@@ -1,68 +1,53 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
-using cleancoderscom.socketserver;
+using System.Threading;
+using System.Text;
+using System.IO;
+
 using NUnit.Framework;
+using cleancoderscom.socketserver;
+
+
+//Conversion from Java to C# 
+//Java Socket replaced with .NET TcpClient
+//Java ServerSocket replaced with .NET TcpListener
+
+//http://stackoverflow.com/questions/209281/c-sharp-equivalent-to-javas-wait-and-notify
+//http://www.codeproject.com/Articles/28785/Thread-synchronization-Wait-and-Pulse-demystified
+//foo.notify() => Monitor.Pulse(foo)
+//foo.notifyAll() => Monitor.PulseAll(foo)
+//foo.wait() =>  Monitor.Wait(foo)
 
 namespace cleancoderscom.tests.socketserver
 {
 
     public class SocketServerTest
     {
-        private FakeSocketService service;
-        private SocketServer server;
-        private int port;
+        private static ClosingSocketService service;
+        private static SocketServer server;
+        private static int port;
 
+        static SocketServerTest()
+        {
+            port = 8042;  
+        }
+
+        //This method is not called by NUnit if there is no tests.
         [SetUp]
         public virtual void setUp()
         {
-            service = new FakeSocketService();
             port = 8042;
-            server = new SocketServer(port, service);
         }
 
-        [TearDown]
-        public virtual void tearDown()
+        public abstract class TestSocketService : SocketService
         {
-            server.stop();
-        }
-
-        [Test]
-        public virtual void instantiate()
-        {
-            Assert.AreEqual(port, server.Port);
-            Assert.AreEqual(service, server.Service);
-        }
-
-        [Test]
-        public virtual void canStartAndStopServer()
-        {
-            server.start();
-            Assert.IsTrue(server.Running);
-            server.stop();
-            Assert.IsFalse(server.Running);
-        }
-
-        [Test]
-        public virtual void acceptsAnIncomingConnection()
-        {
-            server.start();
-            var t = new TcpClient("localhost", port);
-            server.stop();
-
-            Assert.AreEqual(1, service.connections);
-        }
-
-
-        public class FakeSocketService : SocketService
-        {
-            public int connections;
-
             public virtual void serve(TcpClient s)
             {
-                connections++;
                 try
                 {
+                    doService(s);
+                    Notify();
                     s.Close();
                 }
                 catch (Exception e)
@@ -71,7 +56,171 @@ namespace cleancoderscom.tests.socketserver
                     Console.Write(e.StackTrace);
                 }
             }
-        }
-    }
 
+            bool block = true;
+
+            private void Notify()
+            {
+                lock (this)
+                {
+                    block = false;
+                    Monitor.Pulse(this);
+                }
+            }
+
+            public void Wait()
+            {
+                lock (this)
+                {
+                    while (block)
+                    {
+                        Monitor.Wait(this);
+                    }
+                    block = true;
+                }
+
+            }
+
+            protected internal abstract void doService(TcpClient s);
+        }
+
+        #region WithClosingSocketService
+
+        public class ClosingSocketService : TestSocketService
+        {
+            public int connections;
+            protected internal override void doService(TcpClient s)
+            {
+                connections++;
+            }
+        }
+
+        public class WithClosingSocketService
+        {
+
+            [SetUp]
+            public virtual void setUp()
+            {
+                SocketServerTest.service = new ClosingSocketService();
+                SocketServerTest.server = new SocketServer(SocketServerTest.port, SocketServerTest.service);
+            }
+
+            [TearDown]
+            public virtual void tearDown()
+            {
+                SocketServerTest.server.stop();
+            }
+
+            [Test]
+            public virtual void instantiate()
+            {
+                Assert.AreEqual(SocketServerTest.port, SocketServerTest.server.Port);
+                Assert.AreEqual(service, SocketServerTest.server.Service);
+            }
+
+            [Test]
+            public virtual void canStartAndStopServer()
+            {
+                SocketServerTest.server.start();
+                Assert.IsTrue(SocketServerTest.server.Running);
+                SocketServerTest.server.stop();
+                Assert.IsFalse(SocketServerTest.server.Running);
+            }
+
+            [Test]
+            public virtual void acceptsAnIncomingConnection()
+            {
+                SocketServerTest.server.start();
+                var temp = new TcpClient("localhost", SocketServerTest.port);
+                SocketServerTest.service.Wait();
+                SocketServerTest.server.stop();
+
+                Assert.AreEqual(1, SocketServerTest.service.connections);
+            }
+
+            [Test]
+            public virtual void acceptsMultipleIncomingConnections()
+            {
+                SocketServerTest.server.start();
+                var temp = new TcpClient("localhost", SocketServerTest.port);
+                SocketServerTest.service.Wait();
+                
+                temp = new TcpClient("localhost", SocketServerTest.port);
+                SocketServerTest.service.Wait();
+
+                SocketServerTest.server.stop();
+
+                Assert.AreEqual(2, SocketServerTest.service.connections);
+            }
+        } 
+        #endregion
+
+        #region WithReadingSocketService
+
+        public class ReadingSocketService : TestSocketService
+        {
+            public string message;
+
+            protected internal override void doService(TcpClient s)
+            {
+                Stream @is = s.GetStream();
+                StreamReader br = new System.IO.StreamReader(@is);
+                message = br.ReadLine();
+            }
+        }
+
+        public class WithReadingSocketService
+        {
+
+            internal ReadingSocketService readingService;
+
+            [SetUp]
+            public virtual void setup()
+            {
+                readingService = new ReadingSocketService();
+                SocketServerTest.server = new SocketServer(SocketServerTest.port, readingService);
+            }
+
+            [Test]
+            public virtual void canSendAndReceiveData()
+            {
+                SocketServerTest.server.start();
+                TcpClient s = new TcpClient("localhost", SocketServerTest.port);
+                System.IO.Stream os = s.GetStream();
+                var buffer = Encoding.UTF8.GetBytes("hello\n");
+                os.Write(buffer, 0, buffer.Length);
+
+                readingService.Wait();
+                SocketServerTest.server.stop();
+
+                Assert.AreEqual("hello", readingService.message);
+            }
+        } 
+       
+        #endregion
+
+        #region WithEchoSocketService
+        public class EchoSocketService : TestSocketService
+        {
+            protected internal override void doService(TcpClient s)
+            {
+            }
+        }
+
+        public class WithEchoSocketService
+        {
+
+            internal ReadingSocketService readingService;
+            [SetUp]
+            public virtual void setup()
+            {
+            }
+            [Test]
+            [Ignore("Not implemented at Episode 5")]
+            public virtual void canSendAndReceiveData()
+            {
+            }
+        } 
+        #endregion
+    }
 }
