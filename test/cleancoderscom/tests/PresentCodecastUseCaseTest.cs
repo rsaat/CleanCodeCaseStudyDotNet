f965b5efa46fa3f7718ae202760ea3e1ccacc20e
rsaat <rsaat@usa.com>
2015-01-08 16:59:22 -0200

    Refactor Test to use inheritance and nested classes.

--- test/cleancoderscom/tests/PresentCodecastUseCaseTest.cs
+++ test/cleancoderscom/tests/PresentCodecastUseCaseTest.cs
@@ -9,15 +9,12 @@ namespace cleancoderscom.tests.presentation
     using System;
     /// <summary>
     ///   PresentCodecastUseCaseTest parent tests.
-    ///   TODO:Remove nested classes tests or find a better implementation with C#. 
-    ///   This code requires that outer class tests before nested class tests.  
     /// </summary>
     public class PresentCodecastUseCaseTest
     {
 
         private  User user;
         private  PresentCodecastUseCase useCase;
-        private static PresentCodecastUseCaseTest PresentCodecastUseCaseTestInstance;
 
         [SetUp]
         public virtual void setUp()
@@ -25,33 +22,21 @@ namespace cleancoderscom.tests.presentation
             TestSetup.setupContext();
             user = Context.userGateway.save(new User("User"));
             useCase = new PresentCodecastUseCase();
-            PresentCodecastUseCaseTestInstance = this;
         }
 
-        [Test]
-        public virtual void __ThisTestMustBeExecutedBeforeInnerClassTests()
-        {
             
-        }
 
         #region Given No Codecasts tests 
 
-        public class GivenNoCodecasts
-        {
-            private readonly PresentCodecastUseCaseTest outerInstance;
 
-            public GivenNoCodecasts()
-                : this(PresentCodecastUseCaseTestInstance) { }
 
-            public GivenNoCodecasts(PresentCodecastUseCaseTest outerInstance)
+        public class GivenNoCodecasts : PresentCodecastUseCaseTest
             {
-                this.outerInstance = outerInstance;
-            }
 
             [Test]
             public virtual void noneArePresented()
             {
-                IList<PresentableCodecast> presentableCodecasts = outerInstance.useCase.presentCodecasts(outerInstance.user);
+                IList<PresentableCodecast> presentableCodecasts = useCase.presentCodecasts(user);
 
                 Assert.AreEqual(0, presentableCodecasts.Count);
             }
@@ -61,20 +46,11 @@ namespace cleancoderscom.tests.presentation
 
         #region Given One Codecast Tests 
 
-        public class GivenOneCodecast
-        {
-            private readonly PresentCodecastUseCaseTest outerInstance;
 
-            private static GivenOneCodecast GivenOneCodecastInstance;
 
-            public GivenOneCodecast()
-                : this(PresentCodecastUseCaseTestInstance) { }
 
-            public GivenOneCodecast(PresentCodecastUseCaseTest outerInstance)
+        public class GivenOneCodecastContext : PresentCodecastUseCaseTest
             {
-                this.outerInstance = outerInstance;
-                GivenOneCodecastInstance = this;
-            }
 
             internal Codecast codecast;
 
@@ -85,6 +61,8 @@ namespace cleancoderscom.tests.presentation
 
             }
 
+            public class GivenOneCodecast : GivenOneCodecastContext
+            {
             [Test]
             public virtual void oneIsPresented()
             {
@@ -92,100 +70,83 @@ namespace cleancoderscom.tests.presentation
                 DateTime now = (new DateTime(2014, 5, 19));
                 codecast.PublicationDate = now;
                 Context.codecastGateway.save(codecast);
-                IList<PresentableCodecast> presentableCodecasts = outerInstance.useCase.presentCodecasts(outerInstance.user);
+                    IList<PresentableCodecast> presentableCodecasts = useCase.presentCodecasts(user);
                 Assert.AreEqual(1, presentableCodecasts.Count);
                 PresentableCodecast presentableCodecast = presentableCodecasts[0];
                 Assert.AreEqual("Some Title", presentableCodecast.title);
                 Assert.AreEqual("5/19/2014", presentableCodecast.publicationDate.ToString());
             }
 
-            public class GivenNoLicenses
-            {
-                private readonly PresentCodecastUseCaseTest.GivenOneCodecast outerInstance;
+            }
 
-                public GivenNoLicenses()
-                    : this(GivenOneCodecastInstance) { }
 
-                public GivenNoLicenses(PresentCodecastUseCaseTest.GivenOneCodecast outerInstance)
+            public class GivenNoLicenses : GivenOneCodecastContext
                 {
-                    this.outerInstance = outerInstance;
-                }
+              
                 [Test]
                 public virtual void userCannotViewCodecast()
                 {
-                    Assert.IsFalse(outerInstance.outerInstance.useCase.isLicensedFor(LT.VIEWING, outerInstance.outerInstance.user, outerInstance.codecast));
+                    Assert.IsFalse(useCase.isLicensedFor(LT.VIEWING, user, codecast));
                 }
                 [Test]
                 public virtual void presentedCodecastShowsNotViewable()
                 {
-                    IList<PresentableCodecast> presentableCodecasts = outerInstance.outerInstance.useCase.presentCodecasts(outerInstance.outerInstance.user);
+                    IList<PresentableCodecast> presentableCodecasts = useCase.presentCodecasts(user);
                     PresentableCodecast presentableCodecast = presentableCodecasts[0];
                     Assert.IsFalse(presentableCodecast.isViewable);
                 }
             }
 
-            public class GivenOneViewingLicenseForTheUser
+            public class GivenOneViewingLicenseForTheUser : GivenOneCodecastContext
             {
-                private readonly PresentCodecastUseCaseTest.GivenOneCodecast outerInstance;
+                
 
-                public GivenOneViewingLicenseForTheUser()
-                    : this(GivenOneCodecastInstance) { }
 
-                public GivenOneViewingLicenseForTheUser(PresentCodecastUseCaseTest.GivenOneCodecast outerInstance)
-                {
-                    this.outerInstance = outerInstance;
-                }
                 internal License viewLicense;
                 [SetUp]
                 public virtual void setupLicense()
                 {
-                    viewLicense = new License(LT.VIEWING, outerInstance.outerInstance.user, outerInstance.codecast);
+                    viewLicense = new License(LT.VIEWING, user, codecast);
                     Context.licenseGateway.save(viewLicense);
                 }
                 [Test]
                 public virtual void userCanViewCodecast()
                 {
-                    Assert.IsTrue(outerInstance.outerInstance.useCase.isLicensedFor(LT.VIEWING, outerInstance.outerInstance.user, outerInstance.codecast));
+                    Assert.IsTrue(useCase.isLicensedFor(LT.VIEWING, user, codecast));
                 }
                 [Test]
                 public virtual void unlicensedUserCannotViewOtherUsersCodecast()
                 {
                     User otherUser = new User("otherUser");
                     Context.userGateway.save(otherUser);
-                    Assert.IsFalse(outerInstance.outerInstance.useCase.isLicensedFor(LT.VIEWING, otherUser, outerInstance.codecast));
+                    Assert.IsFalse(useCase.isLicensedFor(LT.VIEWING, otherUser, codecast));
                 }
                 [Test]
                 public virtual void presentedCodecastIsViewable()
                 {
-                    Context.licenseGateway.save(new License(LT.VIEWING, outerInstance.outerInstance.user, outerInstance.codecast));
-                    IList<PresentableCodecast> presentableCodecasts = outerInstance.outerInstance.useCase.presentCodecasts(outerInstance.outerInstance.user);
+                    Context.licenseGateway.save(new License(LT.VIEWING, user, codecast));
+                    IList<PresentableCodecast> presentableCodecasts = useCase.presentCodecasts(user);
                     PresentableCodecast presentableCodecast = presentableCodecasts[0];
                     Assert.IsTrue(presentableCodecast.isViewable);
                 }
             }
 
-            public class GivenOneDownloadLicenseForTheUser
+            public class GivenOneDownloadLicenseForTheUser : GivenOneCodecastContext
             {
-                private readonly PresentCodecastUseCaseTest.GivenOneCodecast outerInstance;
+            
 
-                public GivenOneDownloadLicenseForTheUser()
-                    : this(GivenOneCodecastInstance) { }
 
-                public GivenOneDownloadLicenseForTheUser(PresentCodecastUseCaseTest.GivenOneCodecast outerInstance)
-                {
-                    this.outerInstance = outerInstance;
-                }
                 internal License downloadLicense;
                 [SetUp]
                 public virtual void setupDownloadLicense()
                 {
-                    downloadLicense = new License(LT.DOWNLOADING, outerInstance.outerInstance.user, outerInstance.codecast);
+                    downloadLicense = new License(LT.DOWNLOADING, user, codecast);
                     Context.licenseGateway.save(downloadLicense);
                 }
                 [Test]
                 public virtual void presentedCodecastIsDownloadable()
                 {
-                    IList<PresentableCodecast> presentableCodecasts = outerInstance.outerInstance.useCase.presentCodecasts(outerInstance.outerInstance.user);
+                    IList<PresentableCodecast> presentableCodecasts = useCase.presentCodecasts(user);
                     PresentableCodecast presentableCodecast = presentableCodecasts[0];
                     Assert.IsTrue(presentableCodecast.isDownloadable);
                     Assert.IsFalse(presentableCodecast.isViewable);
